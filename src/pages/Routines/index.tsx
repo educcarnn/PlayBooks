7e4621dcedda2992977763d7fe3a66da623cbb8f
educcarnn <educcarnn@gmail.com>
2022-10-24 09:29:39 -0300

    feat: add temporizador

--- src/pages/Routines/index.tsx
+++ src/pages/Routines/index.tsx
@@ -1,26 +1,27 @@
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import { useStopwatch } from "react-timer-hook";
 import { RoutinesDiv } from "./style";
 
 function Routines() {
-  const handleClear = () => {
-
-  
-  }
+  const { seconds, minutes, hours, days, isRunning, start, pause, reset } =
+    useStopwatch({autoStart: false});
+  const handleClear = () => {};
   let history = useHistory();
   const handleHome = () => {
     history.push("/contents");
   };
-  interface nameProps{
+  interface nameProps {
     name: string;
     autor: string;
   }
   const [nameBook, setNameBook] = useState();
 
- 
   return (
     <RoutinesDiv>
-      <div className="play" onClick={handleHome}>Play Book</div>
+      <div className="play" onClick={handleHome}>
+        Play Book
+      </div>
       <div>
         Liste as suas leituras e temporize o seu tempo, para melhorar seu foco e
         concentração
@@ -31,10 +32,23 @@ function Routines() {
         <button>Digite o autor do livro</button>
         <button onClick={handleClear}>Excluir Livro</button>
       </div>
-    
-      <div></div>
+
+      <div>
+        <div>Temporizador</div>
+        <div style={{ textAlign: "center" }}>
+          <h1>react-timer-hook</h1>
+          <p>Stopwatch Demo</p>
+          <div style={{ fontSize: "100px" }}>
+            <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:
+            <span>{seconds}</span>
+          </div>
+          <p>{isRunning ? "Running" : "Not running"}</p>
+          <button onClick={start}>Start</button>
+          <button onClick={pause}>Pause</button>
+          {/* <button onClick={reset}>Reset</button>  */}
+        </div>
+      </div>
     </RoutinesDiv>
   );
-  
 }
 export default Routines;
